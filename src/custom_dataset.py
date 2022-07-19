fcfd663c497bff729133c65b1859d03a1d3e46f3
poludmik <poludmik@fel.cvut.cz>
2022-07-19 12:23:46 +0200

    Finished dataset of 5 genres: classical, pop, rap, lo-fi and METAL!!!.

--- src/custom_dataset.py
+++ src/custom_dataset.py
@@ -1,5 +1,6 @@
 import os
 from pydub import AudioSegment
+from pydub.utils import make_chunks
 import csv
 
 
@@ -14,7 +15,7 @@ class songsManager:
         for filename in os.listdir(foldername):
             f = os.path.join(foldername, filename)
             if os.path.isfile(f):
-                print(f)
+                print(filename)
                 song = AudioSegment.from_file(f)
 
                 song_length_sec = song.duration_seconds
@@ -28,18 +29,67 @@ class songsManager:
                 mono_audios[0].export(to_folder + "/" + filename[:-4] + ".wav", format="wav")
 
 
+    @staticmethod
+    def cut_30s_to_3s_and_store_with_labels(from_folder, label, to_folder, csv_file):
 
+        chunk_length_ms = 3000
+
+        with open(csv_file, newline='') as f:
+            reader = csv.reader(f)
+            csv_rows = list(reader)
+
+        for filename in os.listdir(from_folder):
+            f = os.path.join(from_folder, filename)
+            if os.path.isfile(f):
+                myaudio = AudioSegment.from_file(f, "wav")
+                chunks = make_chunks(myaudio, chunk_length_ms)
+                chunks = chunks[0:10]
+                if len(chunks) < 10:
+                    print("Chunks:", len(chunks), filename)
+
+                for i, chunk in enumerate(chunks):
+                    track_name = str(label) + "_" + filename[:-4] + "_" + str(i) + ".wav"
+                    chunk_name = to_folder + "/" + track_name
+                    chunk.export(chunk_name, format="wav")
+                    csv_rows.append([track_name, str(label)])
+
+
+        with open(csv_file, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerows(csv_rows)
 
 
 if __name__ == "__main__":
 
-    labels = {0: "classical", 1: "pop", 2: "rap", 3: "metal"}
+    labels = {0: "classical", 1: "pop", 2: "rap", 3: "lofi", 4:"metal"}
 
-    folder_classical = "C:/Users/micha/homeworks/personal/Music/data/mishas_dataset/downloaded_songs/rap"
-    end_folder = "C:/Users/micha/homeworks/personal/Music/data/mishas_dataset/downloaded_songs/rap_30s"
-    csv_doc = "C:/Users/micha/homeworks/personal/Music/data/mishas_dataset/labels.csv"
+    # Trim to 30 seconds
+    """""""""
+    # folder_classical = "C:/Users/micha/homeworks/personal/Music/data/mishas_dataset/downloaded_songs/lofi"
+    # end_folder = "C:/Users/micha/homeworks/personal/Music/data/mishas_dataset/downloaded_songs/lofi_30s"
+    # songsManager.add_songs_from_folder_to_dataset(folder_classical, end_folder)
+    """""""""
 
-    songsManager.add_songs_from_folder_to_dataset(folder_classical, end_folder)
+    # Split to 3s tracks and store them with labels
+    label = 3
+    csv_doc = "C:/Users/micha/homeworks/personal/Music/data/mishas_dataset/labels_" + labels[label] + ".csv"
+    start_folder = "C:/Users/micha/homeworks/personal/Music/data/mishas_dataset/downloaded_songs/" + labels[label] +"_30s"
+    end_folder = "C:/Users/micha/homeworks/personal/Music/data/mishas_dataset/downloaded_songs/" + labels[label] +"_3s"
+
+    songsManager.cut_30s_to_3s_and_store_with_labels(start_folder, label, end_folder, csv_doc)
+
+    # header = ["songname", "label"]
+    # data = [["hammer_smashed_face", "500"], ["yellow_submarine", "700"]]
+    # with open(csv_doc, 'w', newline='') as f:
+    #     writer = csv.writer(f)
+    #
+    #     # write the header
+    #     writer.writerow(header)
+    #
+    #     # write multiple rows
+    #     writer.writerows(data)
+
+
 
 
 
